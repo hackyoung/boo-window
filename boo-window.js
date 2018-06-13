39cac417a935e9dfe420ca8dd22c992c6a9a6f7b
hackyoung <hackyoung@163.com>
2018-06-13 18:10:41 +0800

    移出事件, moving, resizing, moved, resized, 新增事件, move, resize, move-start, resize-start, move-end, resize-end

--- boo-window.js
+++ boo-window.js
@@ -28,96 +28,96 @@ const BaseWindow = mixinBehaviors([NeonAnimationRunnerBehavior], PolymerElement)
 export class BooWindow extends BaseWindow {
   static get template() {
     return html`
-    <style>
-      :host {
-        position: fixed;
-        display: block;
-        z-index: 100;
-        box-sizing: border-box;
-      }
-      .wrapper {
-        background-color: white;
-        @apply --boo-window-container;
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        position: relative;
-      }
-      .wrapper>div {
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-      }
-      .wrapper>a {
-        position: absolute;
-        z-index: 10000;
-        background-color: rgba(0, 0, 0, 0);
-        display: block;
-        width: 10px;
-        height: 10px;
-      }
-      .wrapper>a.tl {
-        top: 0px;
-        left: 0px;
-      }
-      .wrapper>a.tl:hover {
-        cursor: nw-resize;
-      }
-      .wrapper>a.tr:hover {
-        cursor: ne-resize;
-      }
-      .wrapper>a.br:hover {
-        cursor: se-resize;
-      }
-      .wrapper>a.bl:hover {
-        cursor: sw-resize;
-      }
-      :host([no-resize]) .wrapper>a.tl:hover,
-      :host([no-resize]) .wrapper>a.tr:hover,
-      :host([no-resize]) .wrapper>a.br:hover,
-      :host([no-resize]) .wrapper>a.bl:hover {
-        cursor: default;
-      }
-      .wrapper>a.tr {
-        top: 0px;
-        right: 0px;
-      }
-      .wrapper>a.bl {
-        bottom: 0px;
-        left: 0px;
-      }
-      .wrapper>a.br {
-        bottom: 0px;
-        right: 0px;
-      }
-      #shadow {
-        opacity: 0.4;
-        background-color: white;
-        top: 0px;
-        left: 0px;
-        width: 100vw;
-        height: 100vh;
-        @apply --boo-window-shadow;
-        position: fixed;
-      }
-    </style>
-    <div id="shadow"></div>
-    <iron-media-query query="(max-width: 767px)" query-matches="{{smallScreen}}"></iron-media-query>
-    <div class="wrapper">
-      <a class="tl" on-mousedown="_onResizeTriggerMouseDown"></a>
-      <a class="tr" on-mousedown="_onResizeTriggerMouseDown"></a>
-      <a class="bl" on-mousedown="_onResizeTriggerMouseDown"></a>
-      <a class="br" on-mousedown="_onResizeTriggerMouseDown"></a>
-      <div>
-        <div id="move-trigger" on-mousedown="_onMoveTriggerMouseDown">
-          <slot name="move-trigger"></slot>
-        </div>
-        <div id="content">
-          <slot name="content"></slot>
+      <style>
+        :host {
+          position: fixed;
+          display: block;
+          z-index: 100;
+          box-sizing: border-box;
+        }
+        .wrapper {
+          background-color: white;
+          @apply --boo-window-container;
+          width: 100%;
+          height: 100%;
+          overflow: hidden;
+          position: relative;
+        }
+        .wrapper>div {
+          width: 100%;
+          height: 100%;
+          overflow: auto;
+        }
+        .wrapper>a {
+          position: absolute;
+          z-index: 10000;
+          background-color: rgba(0, 0, 0, 0);
+          display: block;
+          width: 10px;
+          height: 10px;
+        }
+        .wrapper>a.tl {
+          top: 0px;
+          left: 0px;
+        }
+        .wrapper>a.tl:hover {
+          cursor: nw-resize;
+        }
+        .wrapper>a.tr:hover {
+          cursor: ne-resize;
+        }
+        .wrapper>a.br:hover {
+          cursor: se-resize;
+        }
+        .wrapper>a.bl:hover {
+          cursor: sw-resize;
+        }
+        :host([no-resize]) .wrapper>a.tl:hover,
+        :host([no-resize]) .wrapper>a.tr:hover,
+        :host([no-resize]) .wrapper>a.br:hover,
+        :host([no-resize]) .wrapper>a.bl:hover {
+          cursor: default;
+        }
+        .wrapper>a.tr {
+          top: 0px;
+          right: 0px;
+        }
+        .wrapper>a.bl {
+          bottom: 0px;
+          left: 0px;
+        }
+        .wrapper>a.br {
+          bottom: 0px;
+          right: 0px;
+        }
+        #shadow {
+          opacity: 0.4;
+          background-color: white;
+          top: 0px;
+          left: 0px;
+          width: 100vw;
+          height: 100vh;
+          @apply --boo-window-shadow;
+          position: fixed;
+        }
+      </style>
+      <div id="shadow"></div>
+      <iron-media-query query="(max-width: 767px)" query-matches="{{smallScreen}}"></iron-media-query>
+      <div class="wrapper">
+        <a class="tl" on-mousedown="_onResizeTriggerMouseDown"></a>
+        <a class="tr" on-mousedown="_onResizeTriggerMouseDown"></a>
+        <a class="bl" on-mousedown="_onResizeTriggerMouseDown"></a>
+        <a class="br" on-mousedown="_onResizeTriggerMouseDown"></a>
+        <div>
+          <div id="move-trigger" on-mousedown="_onMoveTriggerMouseDown">
+            <slot name="move-trigger"></slot>
+          </div>
+          <div id="content">
+            <slot name="content"></slot>
+          </div>
         </div>
       </div>
-    </div>
-`;
+    `;
   }
 
   static get is() { return 'boo-window'; }
@@ -176,6 +176,22 @@ export class BooWindow extends BaseWindow {
         observer: '_update',
         notify: true
       },
+      maxWidth: {
+        type: String,
+        observer: '_update',
+      },
+      maxHeight: {
+        type: String,
+        observer: '_update',
+      },
+      minHeight: {
+        type: String,
+        observer: '_update',
+      },
+      minWidth: {
+        type: String,
+        observer: '_update'
+      },
       noResize: {
         type: Boolean,
         value: false,
@@ -216,7 +232,7 @@ export class BooWindow extends BaseWindow {
   }
 
   center() {
-    let rect = this._size();
+    let rect = this.size();
     return {
       x: (BooWindow.screenWidth - rect.width) / 2 + this.fixX,
       y: (BooWindow.screenHeight - rect.height) / 2 + this.fixY
@@ -231,7 +247,7 @@ export class BooWindow extends BaseWindow {
   }
 
   top() {
-    let rect = this._size();
+    let rect = this.size();
     return {
       x: (BooWindow.screenWidth - rect.width) / 2 + this.fixX,
       y: this.fixY
@@ -239,7 +255,7 @@ export class BooWindow extends BaseWindow {
   }
 
   topRight() {
-    let rect = this._size();
+    let rect = this.size();
     return {
       x: BooWindow.screenWidth - rect.width + this.fixX,
       y: this.fixY
@@ -247,7 +263,7 @@ export class BooWindow extends BaseWindow {
   }
 
   left() {
-    let rect = this._size();
+    let rect = this.size();
     return {
       x: this.fixX,
       y: (BooWindow.screenHeight - rect.height) / 2 + this.fixY,
@@ -255,7 +271,7 @@ export class BooWindow extends BaseWindow {
   }
 
   right() {
-    let rect = this._size();
+    let rect = this.size();
     return {
       x: BooWindow.screenWidth - rect.width + this.fixX,
       y: (BooWindow.screenHeight - rect.height) / 2 + this.fixY,
@@ -263,7 +279,7 @@ export class BooWindow extends BaseWindow {
   }
 
   bottomLeft() {
-    let rect = this._size();
+    let rect = this.size();
     return {
       x: this.fixX,
       y: BooWindow.screenHeight - rect.height + this.fixY,
@@ -271,7 +287,7 @@ export class BooWindow extends BaseWindow {
   }
 
   bottom() {
-    let rect = this._size();
+    let rect = this.size();
     return {
       x: (BooWindow.screenWidth - rect.width) / 2 + this.fixX,
       y: BooWindow.screenHeight - rect.height + this.fixY,
@@ -279,7 +295,7 @@ export class BooWindow extends BaseWindow {
   }
 
   bottomRight() {
-    let rect = this._size();
+    let rect = this.size();
     return {
       x: BooWindow.screenWidth - rect.width + this.fixX,
       y: BooWindow.screenHeight - rect.height + this.fixY,
@@ -300,31 +316,77 @@ export class BooWindow extends BaseWindow {
     this.update();
   }
 
-  _size() {
+  _w(width) {
+    let w = this._real(width, BooWindow.screenWidth);
+    let maxw = this._maxWidth();
+    let minw = this._minWidth() || 0;
+
+    return Math.max(Math.min(w, maxw), minw);
+  }
+
+  _h(height) {
+    let h = this._real(height, BooWindow.screenHeight);
+    let maxh = this._maxHeight();
+    let minh = this._minHeight() || 0;
+
+    return Math.max(Math.min(h, maxh), minh);
+  }
+
+  size() {
     return {
-      width: Math.min(this._width(), BooWindow.screenWidth),
-      height: Math.min(this._height(), BooWindow.screenHeight)
+      width: this._w(this.width),
+      height: this._h(this.height)
     };
   }
 
+  _minHeight() {
+    if (typeof this.maxHeight == 'undefined') {
+      return null;
+    }
+
+    return this._real(this.minHeight, BooWindow.screenHeight);
+  }
+
+  _minWidth() {
+    if (typeof this.maxWidth == 'undefined') {
+      return null;
+    }
+
+    return this._real(this.minWidth, BooWindow.screenWidth);
+  }
+
+  _maxHeight() {
+    if (typeof this.maxHeight == 'undefined') {
+      return BooWindow.screenHeight;
+    }
+
+    return Math.min(BooWindow.screenHeight, this._real(this.maxHeight, BooWindow.screenHeight));
+  }
+
+  _maxWidth() {
+    if (typeof this.maxWidth == 'undefined') {
+      return BooWindow.screenWidth;
+    }
+
+    return Math.min(BooWindow.screenWidth, this._real(this.maxWidth, BooWindow.screenWidth));
+  }
+
   _height() {
-    if (/px$/.test(this.height)) {
-      return parseFloat(this.height);
-    }
-    if (/vh$/.test(this.height) || /%$/.test(this.height)) {
-      return BooWindow.screenHeight * parseFloat(this.height) / 100;
-    }
-    return parseFloat(this.height);
+    return this._real(this.height, BooWindow.screenHeight);
   }
 
   _width() {
-    if (/px$/.test(this.width)) {
-      return parseFloat(this.width);
+    return this._real(this.width, BooWindow.screenWidth);
+  }
+
+  _real(logical, total) {
+    if (/px$/.test(logical)) {
+      return parseFloat(logical);
     }
-    if (/vw$/.test(this.width) || /%$/.test(this.width)) {
-      return BooWindow.screenWidth * parseFloat(this.width) / 100;
+    if (/vw$/.test(logical) || /%$/.test(logical)) {
+      return total * parseFloat(logical) / 100;
     }
-    return parseFloat(this.width);
+    return parseFloat(logical);
   }
 
   wrapper() {
@@ -389,6 +451,7 @@ export class BooWindow extends BaseWindow {
     this._beginY = this.y;
     let body = document.getElementsByTagName('body')[0];
     body.style.userSelect = 'none';
+    this.dispatchEvent(new CustomEvent('resize-start'));
   }
 
   _onMoveTriggerMouseDown(e) {
@@ -404,19 +467,20 @@ export class BooWindow extends BaseWindow {
     moveTrigger.style.cursor = 'move';
     let body = document.getElementsByTagName('body')[0];
     body.style.userSelect = 'none';
+    this.dispatchEvent(new CustomEvent("move-start"));
   }
 
   _onWindowMouseUp(e) {
     let moveTrigger = this.shadowRoot.querySelector('#move-trigger');
     if (this._moving) {
       this._moving = false;
-      this.dispatchEvent(new CustomEvent('moved'));
+      moveTrigger.style.cursor = 'default';
+      this.dispatchEvent(new CustomEvent('move-end'));
     }
     if (this._resizing) {
       this._resizing = false;
-      this.dispatchEvent(new CustomEvent('resized'));
+      this.dispatchEvent(new CustomEvent('resize-end'));
     }
-    moveTrigger.style.cursor = 'default';
     let body = document.getElementsByTagName('body')[0];
     body.style.userSelect = 'text';
   }
@@ -425,7 +489,7 @@ export class BooWindow extends BaseWindow {
     if (this._moving) {
       this.x = this._beginX + e.screenX - this._beginCursorX;
       this.y = this._beginY + e.screenY - this._beginCursorY;
-      this.dispatchEvent(new CustomEvent('moving'));
+      this.dispatchEvent(new CustomEvent('move'));
     }
     if (!this._resizing) {
       return;
@@ -433,27 +497,55 @@ export class BooWindow extends BaseWindow {
     let x = e.screenX - this._beginCursorX;
     let y = e.screenY - this._beginCursorY;
     let klass = this._resizeTrigger.getAttribute('class').split(' ');
+    let minWidth = this._minWidth() || 0;
+    let minHeight = this._minHeight() || 0;
+    let maxWidth = this._maxWidth();
+    let maxHeight = this._maxHeight();
     if (klass.indexOf('tl') != -1) {
-      this.x = this._beginX + x;
-      this.y = this._beginY + y;
-      this.height = this._beginHeight - y;
-      this.width = this._beginWidth - x;
+      let w = this._beginWidth - x;
+      if (w >= minWidth && w <= maxWidth) {
+        this.x = this._beginX + x;
+        this.width = w;
+      }
+      let h = this._beginHeight - y;
+      if (h >= minHeight && h <= maxHeight) {
+        this.y = this._beginY + y;
+        this.height = h;
+      }
     }
     if (klass.indexOf('tr') != -1) {
-      this.y = this._beginY + y;
-      this.height = this._beginHeight - y;
-      this.width = this._beginWidth + x;
+      let w = this._beginWidth + x;
+      if (w >= minWidth && w <= maxWidth) {
+        this.width = w;
+      }
+      let h = this._beginHeight - y;
+      if (h >= minHeight && h <= maxHeight) {
+        this.y = this._beginY + y;
+        this.height = h;
+      }
     }
     if (klass.indexOf('bl') != -1) {
-      this.x = this._beginX + x;
-      this.height = this._beginHeight + y;
-      this.width = this._beginWidth - x;
+      let w = this._beginWidth - x;
+      if (w >= minWidth && w <= maxWidth) {
+        this.x = this._beginX + x;
+        this.width = this._beginWidth - x;
+      }
+      let h = this._beginHeight + y;
+      if (h >= minHeight && h <= maxHeight) {
+        this.height = h;
+      }
     }
     if(klass.indexOf('br') != -1) {
-      this.height = this._beginHeight + y;
-      this.width = this._beginWidth + x;
+      let h = this._beginHeight + y;
+      if (h >= minHeight && h <= maxHeight) {
+        this.height = h;
+      }
+      let w = this._beginWidth + x;
+      if (w >= minWidth && w <= maxWidth) {
+        this.width = w;
+      }
     }
-    this.dispatchEvent(new CustomEvent('resizing'));
+    this.dispatchEvent(new CustomEvent('resize'));
   }
 
   _update() {
@@ -469,8 +561,9 @@ export class BooWindow extends BaseWindow {
     }
     this.style.left = this.x + 'px';
     this.style.top = this.y + 'px';
-    this.style.height = Math.min(this._height(), BooWindow.screenHeight) + 'px';
-    this.style.width = Math.min(this._width(), BooWindow.screenWidth) + 'px';
+    let size = this.size();
+    this.style.height = size['height'] + 'px';
+    this.style.width = size['width'] + 'px';
     this.dispatchEvent(new CustomEvent('update'));
   }
 
